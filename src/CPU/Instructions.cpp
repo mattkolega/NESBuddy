002aa2032d4948b688625c02e0e366e970ccc6f4
Matthew Kolega <12107095+mattkolega@users.noreply.github.com>
2024-02-01 19:17:58 +1100

    Implement status flag changes

--- src/CPU/Instructions.cpp
+++ src/CPU/Instructions.cpp
@@ -1,6 +1,36 @@
-#include "Instructions.h"
+#include "CPU.h"
 
-namespace 6502Instructs
+void CPU::CLC()
 {
-    
+    processorStatus.reset(static_cast<size_t>(Flags::carryFlag));
+}
+
+void CPU::CLD()
+{
+    processorStatus.reset(static_cast<size_t>(Flags::decimalMode));
+}
+
+void CPU::CLI()
+{
+    processorStatus.reset(static_cast<size_t>(Flags::interruptDisable));
+}
+
+void CPU::CLV()
+{
+    processorStatus.reset(static_cast<size_t>(Flags::overflowFlag));
+}
+
+void CPU::SEC()
+{
+    processorStatus.set(static_cast<size_t>(Flags::carryFlag));
+}
+
+void CPU::SED()
+{
+    processorStatus.set(static_cast<size_t>(Flags::decimalMode));
+}
+
+void CPU::SEI()
+{
+    processorStatus.set(static_cast<size_t>(Flags::interruptDisable));
 }
