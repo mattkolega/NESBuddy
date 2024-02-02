f14e2760fc8b438886cea7a29463a56c63060a06
Matthew Kolega <12107095+mattkolega@users.noreply.github.com>
2024-02-02 11:06:51 +1100

    Implement register transfer instructions

--- src/CPU/Instructions.cpp
+++ src/CPU/Instructions.cpp
@@ -1,5 +1,73 @@
 #include "CPU.h"
 
+/**
+ * Register Transfers
+*/
+
+void CPU::TAX()
+{
+    indexX = accumulator;
+
+    if (indexX == 0) {
+        processorStatus.set(static_cast<size_t>(Flags::zeroFlag));
+    }
+
+    bool isNegative = std::bitset<8>(indexX).test(7);
+
+    if (isNegative) {
+        processorStatus.set(static_cast<size_t>(Flags::negativeFlag));
+    }
+}
+
+void CPU::TAY()
+{
+    indexY = accumulator;
+
+    if (indexY == 0) {
+        processorStatus.set(static_cast<size_t>(Flags::zeroFlag));
+    }
+
+    bool isNegative = std::bitset<8>(indexY).test(7);
+
+    if (isNegative) {
+        processorStatus.set(static_cast<size_t>(Flags::negativeFlag));
+    }
+}
+
+void CPU::TXA()
+{
+    accumulator = indexX;
+
+    if (accumulator == 0) {
+        processorStatus.set(static_cast<size_t>(Flags::zeroFlag));
+    }
+
+    bool isNegative = std::bitset<8>(accumulator).test(7);
+
+    if (isNegative) {
+        processorStatus.set(static_cast<size_t>(Flags::negativeFlag));
+    }
+}
+
+void CPU::TYA()
+{
+    accumulator = indexY;
+
+    if (accumulator == 0) {
+        processorStatus.set(static_cast<size_t>(Flags::zeroFlag));
+    }
+
+    bool isNegative = std::bitset<8>(accumulator).test(7);
+
+    if (isNegative) {
+        processorStatus.set(static_cast<size_t>(Flags::negativeFlag));
+    }   
+}
+
+/**
+ * Status Flag Changes
+*/
+
 void CPU::CLC()
 {
     processorStatus.reset(static_cast<size_t>(Flags::carryFlag));
