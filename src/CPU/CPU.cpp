5c76d322822ce6a1847b2a8172bba8da60af60a1
Matthew Kolega <12107095+mattkolega@users.noreply.github.com>
2024-02-04 21:12:37 +1100

    Fix switch statement scope issues

--- src/CPU/CPU.cpp
+++ src/CPU/CPU.cpp
@@ -27,51 +27,59 @@ uint8_t CPU::getValue(AddrMode addressMode)
     switch(addressMode) 
     {
         case AddrMode::absolute:
+        {
             uint8_t byteOne = nes->memory[pc];
             pc++;
             uint8_t byteTwo = nes->memory[pc];
             pc++;
             uint16_t address = (byteTwo << 8) | byteOne;
             return nes->memory[address];
-
+        }
         case AddrMode::accumulator:
+        {
             return accumulator;
-
+        }
         case AddrMode::immediate:
+        {
             uint8_t value = pc;
             pc++;
             return value;
-        
+        }
         case AddrMode::indirect:
+        {
             uint8_t byteOne = nes->memory[pc];
             pc++;
             uint8_t byteTwo = nes->memory[pc];
             pc++;
             uint16_t address = (byteTwo << 8) | byteOne;
             return address;
-
+        }
         case AddrMode::zeroPage:
+        {
             uint8_t address = nes->memory[pc];
             pc++;
             return nes->memory[address];
-
+        }
         case AddrMode::absoluteX:
+        {
             uint8_t byteOne = nes->memory[pc];
             pc++;
             uint8_t byteTwo = nes->memory[pc];
             pc++;
             uint16_t address = ((byteTwo << 8) | byteOne) + indexX;
             return nes->memory[address];
-
+        }
         case AddrMode::absoluteY:
+        {
             uint8_t byteOne = nes->memory[pc];
             pc++;
             uint8_t byteTwo = nes->memory[pc];
             pc++;
             uint16_t address = ((byteTwo << 8) | byteOne) + indexY;
             return nes->memory[address];
-
+        }
         case AddrMode::indexIndirect:
+        {
             uint8_t address = (nes->memory[pc] + indexX);
             pc++;
             uint8_t byteOne = nes->memory[address];
@@ -80,8 +88,9 @@ uint8_t CPU::getValue(AddrMode addressMode)
             address++;
             uint16_t targetAddress = (byteTwo << 8) | byteOne;
             return nes->memory[targetAddress];
-
+        }
         case AddrMode::indirectIndex:
+        {
             uint16_t address = (nes->memory[pc] + indexY);
             pc++;
             uint8_t byteOne = nes->memory[address];
@@ -90,17 +99,19 @@ uint8_t CPU::getValue(AddrMode addressMode)
             address++;
             uint16_t targetAddress = (byteTwo << 8) | byteOne;
             return nes->memory[targetAddress];
-
+        }
         case AddrMode::zeroPageX:
+        {
             uint8_t address = nes->memory[pc] + indexX;
             pc++;
             return nes->memory[address];
-            
+        }
         case AddrMode::zeroPageY:
+        {
             uint8_t address = nes->memory[pc] + indexY;
             pc++;
             return nes->memory[address];
-
+        }
         default:
             break;
     }
