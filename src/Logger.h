fdba0035e3275776f2bfa413a7dea8a0ec34c6ab
Matthew Kolega <12107095+mattkolega@users.noreply.github.com>
2024-08-20 19:05:48 +1000

    Add print function which accepts multiple arguments

--- src/Logger.h
+++ src/Logger.h
@@ -1,6 +1,15 @@
 #pragma once
 
+#include <sstream>
 #include <string_view>
+
+enum class MessageLevel 
+{
+    DEBUG,
+    INFO,
+    WARNING,
+    ERROR,
+};
 
 namespace Logger 
 {
@@ -8,4 +17,29 @@ namespace Logger
     void printInfo(std::string_view message);
     void printWarning(std::string_view message);
     void printError(std::string_view message);
+
+    // High level print function which allows for variadic arguments
+    template<class... Args>
+    void print(MessageLevel messageLevel, const Args&... args)
+    {
+        std::ostringstream message;
+        (message << ... << args);  // Expand arguments and send all to the message stream
+
+        switch (messageLevel) {
+            case (MessageLevel::DEBUG):
+                printDebug(message.str());
+                break;
+            case (MessageLevel::INFO):
+                printInfo(message.str());
+                break;
+            case (MessageLevel::WARNING):
+                printWarning(message.str());
+                break;
+            case (MessageLevel::ERROR):
+                printError(message.str());
+                break;
+            default:
+                break;
+        }
+    }
 }
